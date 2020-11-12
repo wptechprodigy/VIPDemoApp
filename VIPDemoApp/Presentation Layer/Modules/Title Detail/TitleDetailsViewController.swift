2e3bbf770dd61d7a4a1e1ba34e0eda08ba3f6ea6
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-11-12 11:59:55 +0100

    title details controller and view

---
+++ VIPDemoApp/Presentation Layer/Modules/Title Detail/TitleDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TitleDetailViewController.swift
+//  VIPDemoApp
+//
+//  Created by waheedCodes on 12/11/2020.
+//
+
+import UIKit
+
+class TitleDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
