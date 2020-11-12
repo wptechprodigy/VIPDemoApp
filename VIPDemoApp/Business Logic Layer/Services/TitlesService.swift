2b9079bf657e50f54ffec16c5d9da9bb4d38266f
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-11-12 08:28:22 +0100

    add titles interaction

---
+++ VIPDemoApp/Business Logic Layer/Services/TitlesService.swift
@@ -0,0 +1,18 @@
+//
+//  TitlesService.swift
+//  VIPDemoApp
+//
+//  Created by waheedCodes on 10/11/2020.
+//
+
+import UIKit
+
+protocol TitlesService: class {
+    
+}
+
+class TitlesServiceImplementation: TitlesService {
+    func getTitles() {
+        
+    }
+}
